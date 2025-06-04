f600741b927684da78ce8348d10ad1da3a285d71
Satyamskillz <satyamskillz@gmail.com>
2025-06-05 00:09:59 +0530

    refactor: optimize className handling in WidgetButton component using clsx

--- src/components/WidgetButton/index.tsx
+++ src/components/WidgetButton/index.tsx
@@ -1,22 +1,27 @@
-import useRoastWidget from "../../hooks/useRoastWidget";
 import defaultCustomize from "../../utils/defaultCustomize";
 import { buttonElmentClassName } from "../WidgetProvider";
+import useRoastWidget from "../../hooks/useRoastWidget";
+import clsx from "clsx";
 import "./styles.css";
 
 function WidgetButton() {
 	const { active, customize, toggleActive, disable } = useRoastWidget();
-
 	if (disable) return null;
 
 	return (
 		<div
 			data-direction={customize?.island?.direction || defaultCustomize.island?.direction}
-			className={`${buttonElmentClassName} ${customize?.island?.className || ""}`}
+			className={clsx(buttonElmentClassName, customize?.island?.className)}
 			onClick={toggleActive}
 		>
 			<p>{customize?.island?.label || defaultCustomize.island?.label}</p>
-			<div className="switch-btn" data-active={active}>
-				<span className="circle" />
+			<div
+				className={clsx("switch-btn", customize?.island?.switchButton?.className)}
+				data-active={active}
+			>
+				<span
+					className={clsx("circle", customize?.island?.switchButton?.thumb?.className)}
+				/>
 			</div>
 		</div>
 	);
