256f6c2bd7888cb45f4cf87052f081a2a00cb491
Roastnest <team.roastnest@gmail.com>
2025-08-14 02:12:41 +0530

    feat: add setUser to useReactRoast for improved user state management

--- src/hooks/useReactRoast.tsx
+++ src/hooks/useReactRoast.tsx
@@ -2,13 +2,14 @@ import { avoidElementClassName } from "../utils/classNames";
 import useRoastWidget from "./useRoastWidget";
 
 const useReactRoast = () => {
-    const { active, toggleActive, setIslandVisiblity } = useRoastWidget();
+    const { active, toggleActive, setIslandVisiblity, setUser } = useRoastWidget();
 
     return {
         isWidgetActive: active,
         toggleWidget: toggleActive,
         avoidElementClassName,
         setIslandVisiblity,
+        setUser,
     };
 };
 
